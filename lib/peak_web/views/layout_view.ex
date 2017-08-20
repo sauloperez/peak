901be426f58e14548cd162593abd1fd3f2e20f83
Pau Perez <saulopefa@gmail.com>
2017-08-20 21:28:28 +0200

    Set basic styles for navigation links

--- lib/peak_web/views/layout_view.ex
+++ lib/peak_web/views/layout_view.ex
@@ -1,3 +1,16 @@
 defmodule PeakWeb.LayoutView do
   use PeakWeb, :view
+
+  def title do
+    "Peak"
+  end
+
+  def active_class(conn, path) do
+    current_path = Path.join(["/" | conn.path_info])
+    if path == current_path do
+      "active"
+    else
+      nil
+    end
+  end
 end
